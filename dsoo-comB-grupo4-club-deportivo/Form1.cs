14a348169a6425f64d45b0220352f87e964e6428
Lucas Luccaroni <lucas_luccaroni@hotmail.com.ar>
2025-10-09 11:10:42 -0300

    Add project files.

---
+++ dsoo-comB-grupo4-club-deportivo/Form1.cs
@@ -0,0 +1,10 @@
+namespace dsoo_comB_grupo4_club_deportivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
