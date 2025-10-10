536fe9302dcbeea9350f4293465cdb0917dd8677
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2025-10-10 01:53:53 -0300

    Agrego script de la db.

---
+++ dsoo-club-deportivo-conexiondb.sql
@@ -0,0 +1,60 @@
+/*
+Comision B
+Grupo 4
+Integrantes:
+	- Faigenbom, Ivan
+	- Flores, Mauro
+	- Grau, Judith
+	- Jara, Matias
+	- Luccaroni, Lucas
+*/
+
+DROP DATABASE IF EXISTS Dsoo_club_deportivo;
+CREATE DATABASE Dsoo_club_deportivo;
+USE Dsoo_club_deportivo;
+
+-- Roles de usuarios que usen el sistema
+CREATE TABLE Roles(
+RolUsu INT,
+NomRol VARCHAR(30),
+CONSTRAINT pk_roles PRIMARY KEY (RolUsu)
+);
+
+INSERT INTO ROLES VALUES
+(999, "Administrador");
+
+-- Usuarios que usan el sistema
+CREATE TABLE Usuario(
+CodUsu INT AUTO_INCREMENT,
+NombreUsu VARCHAR(20),
+PassUsu VARCHAR(15),
+RolUsu INT,
+Activo BOOLEAN DEFAULT TRUE,
+CONSTRAINT pk_usuario PRIMARY KEY (CodUsu),
+CONSTRAINT fk_usuario FOREIGN KEY (RolUsu) REFERENCES Roles (RolUsu)
+);
+
+INSERT INTO Usuario (CodUsu, NombreUsu, PassUsu, RolUsu) VALUES
+(001, "ivan.faigenbom", "ivan123", 999),
+(002, "mauro.flores", "mauro123", 999),
+(003, "judith.grau", "judith123", 999),
+(004, "matias.jara", "matias123", 999),
+(005, "lucas.luccaroni", "lucas123", 999);
+
+
+-- StoredProcedure para hacer el Login
+DELIMITER //
+CREATE PROCEDURE IngresoLogin(IN Usu VARCHAR(20), IN Pass VARCHAR(15))
+BEGIN
+	SELECT NomRol
+		FROM Usuario u INNER JOIN Roles r ON u.RolUsu = R.RolUsu
+			WHERE NombreUsu = Usu and PassUsu = Pass
+				AND Activo = 1;
+END//
+
+-- Test de que funciona el StoredProcedure ↓ 
+CALL IngresoLogin("lucas.luccaroni", "lucas123");
+CALL IngresoLogin("ivan.faigenbom", "ivan123");
+SELECT * FROM Usuario;
+
+DELIMITER //
