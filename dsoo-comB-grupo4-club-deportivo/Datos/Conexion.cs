414c14729098552dd4db2559cacc50b21b3b4bb5
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2025-11-10 16:07:45 -0300

    Modificacion a la conexion, ahora se deben ingresar los datos necesarios para establecer la conexion y no estan mas hardcodeados

--- dsoo-comB-grupo4-club-deportivo/Datos/Conexion.cs
+++ dsoo-comB-grupo4-club-deportivo/Datos/Conexion.cs
@@ -20,14 +20,48 @@ namespace dsoo_comB_grupo4_club_deportivo.Datos
 
         private Conexion() // asignamos valores a las variables de conexion
         {
+            // variables usadas para la repeticion de lineas de codigo
+            bool correcto = false;
+            int mensaje;
+
+            // creamos las variables para recibir los datos desde el teclado
+            string T_servidor = "Servidor";
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave";
+
+            // ciclo while para repetir el ingreso de datos
+            // la variable "correcto" la inicializamos para ingresar el ciclo
+
+            while (correcto != true)
+            {
+                // armamos los cuadros de dialogo para el ingreso de datos
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox("Ingrese servidor:", "Datos de instalación MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox("Ingrese puerto:", "Datos de instalación MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox("Ingrese usuario:", "Datos de instalación MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox("Ingrese clave:", "Datos de instalación MySQL");
+
+                // controlamos que los datos ingresados para acceder a MySQL sean correctos
+                mensaje = (int)MessageBox.Show("Su ingreso: SERVIDIR = " + T_servidor + " PUERTO = " + T_puerto + " USUARIO = " + T_usuario + " CLAVE = " + T_clave, "Aviso del sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("Ingrese nuevamente los datos", "Aviso del sistema", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
+
             this.baseDatos = "Dsoo_club_deportivo";
-            //this.baseDatos = "dsoo-club-deportivo";
-            this.servidor = "localhost";
-            this.puerto = "3306";
-            this.usuario = "root";
-            this.clave = "Colondesantafe22.";
+            this.servidor = T_servidor;   // "localhost"; 
+            this.puerto = T_puerto;       // "3306"; 
+            this.usuario = T_usuario;     // "root"; 
+            this.clave = T_clave;         // "Colondesantafe22.";
         }
-        
+
         // proceso de interaccion
         public MySqlConnection CrearConexion()
         {
